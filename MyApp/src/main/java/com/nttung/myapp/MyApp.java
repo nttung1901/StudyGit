499017bbe3ceaf548f2b0835bc5b257ec974819e
tung nguyen <nttung1901@gmail.com>
2024-09-20 15:35:36 +0700

    sua code demo

--- MyApp/src/main/java/com/nttung/myapp/MyApp.java
+++ MyApp/src/main/java/com/nttung/myapp/MyApp.java
@@ -11,6 +11,8 @@ package com.nttung.myapp;
 public class MyApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
+        System.out.println("Hi!");
+
     }
 }
