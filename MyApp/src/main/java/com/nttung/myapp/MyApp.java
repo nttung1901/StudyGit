5e951654fd85cfd6b73d58f7b5102db6ebe61897
tung nguyen <nttung1901@gmail.com>
2024-09-20 15:26:49 +0700

    init project

---
+++ MyApp/src/main/java/com/nttung/myapp/MyApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nttung.myapp;
+
+/**
+ *
+ * @author Tung
+ */
+public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
